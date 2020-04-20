513c03802cc66bf1af4fc727cc992ef122596ff6
rukmini-15 <63964260+rukmini-15@users.noreply.github.com>
2020-04-20 19:28:32 +0530

    Add files via upload

---
+++ python_code.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## This notebook will be mainly used for the IBM's Applied Data Science Capstone Project
+
+# #### @author: 97kb059
+
+# In[1]:
+
+
+import pandas as pd
+import numpy  as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
